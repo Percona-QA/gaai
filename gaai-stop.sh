7b71e444bac40462d0c2d0cf4d8598c0d115b853
Roel Van de Paar <roel.vandepaar@percona.com>
2018-10-13 08:24:33 +1100

    Minor fixes

--- gaai-stop.sh
+++ gaai-stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Created by Roel Van de Paar, Percona LLC
 
-ps -ef | grep 'gaai' | grep -vE "grep|vi " | awk '{print $2}' | xargs kill -9
+ps -ef | grep 'gaai' | grep -vE "grep|vi |gaai-stop" | awk '{print $2}' | xargs kill -9
